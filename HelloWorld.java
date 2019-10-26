968964a073c84acde4f7bc5e3c475f7815173dd1
Ryan <rv17643@essex.ac.uk>
2019-10-26 16:11:39 +0100

    adding my name

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		System.out.println("Ching Hei Pang");
+		System.out.println("Ryan Van Dijck");
 	}
 }
