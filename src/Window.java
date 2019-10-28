6e847616a2180b6fce652700ace0fc8098320bad
rv17643 <rv17643@essex.ac.uk>
2019-10-28 12:57:15 +0000

    Starting to create frame. Put into an intellij project.

--- src/Window.java
+++ src/Window.java
@@ -1,5 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 public class Window extends JFrame {
-
+    TextArea output = new TextArea();
+    JPanel visual = new JPanel();
+    JPanel data = new JPanel();
+    public Window(String name){
+        super(name);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        add(output,BorderLayout.SOUTH);
+        add(visual,BorderLayout.EAST);
+        add(data,BorderLayout.WEST);
+        pack();
+        setSize(800,800);
+        setVisible(true);
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,6 @@
 public class main {
+    public static void main(String[] args){
+        Window window = new Window("Student marks");
+
+    }
 }
