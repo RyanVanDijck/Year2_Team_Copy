87294e07cd83005c5a660f9b8662c08c1c4930a0
rv17643 <rv17643@essex.ac.uk>
2019-11-19 15:33:57 +0000

    adding comments

--- src/Window.java
+++ src/Window.java
@@ -1,17 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
-public class Window extends JFrame {
-    TextArea output = new TextArea();
-    JPanel visual = new JPanel();
-    JPanel data = new JPanel();
+public class Window extends JFrame { //a class which extends the JFrame to add custom elements 
+    TextArea output = new TextArea(); //A text area to display program output 
+    JPanel visual = new JPanel(); // A panel to show visual aspects 
+    JPanel data = new JPanel(); //A spare panel for now 
     public Window(String name){
-        super(name);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        add(output,BorderLayout.SOUTH);
+        super(name); //calling the normal JFrame constructor 
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //The program will close when the window closes 
+        //adding the objects 
+		add(output,BorderLayout.SOUTH);
         add(visual,BorderLayout.EAST);
         add(data,BorderLayout.WEST);
+		//Packing the frame 
         pack();
-        setSize(800,800);
-        setVisible(true);
+        //setting the frame size 
+		setSize(800,800);
+        //making the frame visible 
+		setVisible(true);
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,11 +1,11 @@
 import java.io.IOException;
-
+//main method to run program
 public class main{
     public static void main(String args[]) throws IOException{
-        Read read = new Read();
-        for (student i : read.data) {
-            i.temp_print();
+		Read read = new Read();//creating new read object 
+        for (student i : read.data) //iterating though a list of data  {
+            i.temp_print(); //printing to show that the program works properly 
         }
-        Window window = new Window("Student Marks");
+        Window window = new Window("Student Marks");//creating a window to show content
     }
 }
