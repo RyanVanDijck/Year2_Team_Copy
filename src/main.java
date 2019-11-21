b30a03b5f45715937c3bc59e7905e7b09685d599
rv17643 <rv17643@essex.ac.uk>
2019-11-21 15:08:36 +0000

    fixing a slight mistake

--- src/main.java
+++ src/main.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 //main method to run program
 public class main{
     public static void main(String args[]) throws IOException{
+		Window window = new Window("Student Marks");//creating a window to show content
 		Read read = new Read();//creating new read object 
-        for (student i : read.data) //iterating though a list of data  {
+        for (student i : read.data){ //iterating though a list of data  
             i.temp_print(); //printing to show that the program works properly 
         }
-        Window window = new Window("Student Marks");//creating a window to show content
-
+	}
 }
