fae99d7ce88924b86a5ed23b29e0a71a6f517b51
cp17201 <cp17201@essex.ac.uk>
2019-11-22 15:49:40 +0000

    Editing GUI

---
+++ src/mainWindow.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+/*class to create a frame to display data */
+class mainWindow extends JFrame{
+	TextArea output = new TextArea(); //A text area to display program output
+	JPanel visual = new JPanel(); // A panel to show visual aspects
+	JPanel data = new JPanel();//A spare panel for now
+	JPanel user = new JPanel();
+	JPanel  select = new JPanel();
+	JPanel button = new JPanel();
+
+	JComboBox course = new JComboBox();
+
+
+	public mainWindow(String name){
+		super(name);
+		setSize(800,800);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //The program will close when the window closes
+
+		output.setEditable(false);
+
+		select.setLayout(new GridLayout(4,1));
+		select.add(course);
+
+		user.setLayout(new GridLayout(1,2));
+		user.add(select);
+		user.add(output);
+
+		//adding the objects
+		this.setLayout(new GridLayout(3,2));
+		this.add(visual);
+		this.add(data);
+		this.add(user);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+	}
+}
