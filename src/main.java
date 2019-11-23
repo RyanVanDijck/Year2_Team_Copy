9ff1f6f8a99a62260c2086ed89bd04ca94528722
unknown <rv17643@essex.ac.uk>
2019-11-23 02:20:23 +0000

    Changing the look and feel

--- src/main.java
+++ src/main.java
@@ -1,18 +1,34 @@
 import java.io.IOException;
-
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 //main method to run program
 public class main{
     public static void main(String args[]) throws IOException{
-		mainWindow window = new mainWindow("Student Marks");//creating a window to show content
+      try {
+  			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+  		} catch (ClassNotFoundException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		} catch (InstantiationException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		} catch (IllegalAccessException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		} catch (UnsupportedLookAndFeelException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		}
+    mainWindow window = new mainWindow("Student Marks");//creating a window to show content
 		String file = "data.csv"; //Name of file
         window.setVisible(true);
 		Read read = new Read();//creating new read object
-        
+
         for (student i : read.data){ //iterating though a list of data
             window.output.append(i.ExNo +" "+ i.ce101 + " "+ i.ave_mark+"\n");
             //i.temp_print(); //printing to show that the program works properly
         }
 
-        
+
 	}
 }
