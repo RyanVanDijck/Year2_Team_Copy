d3c966bd4b2e74cb9c61ed6678b76107d474ad0e
rv17643 <rv17643@essex.ac.uk>
2020-02-08 17:54:53 +0000

    Adding comments

--- src/GraphFrame.java
+++ src/GraphFrame.java
@@ -10,10 +10,14 @@ import java.util.Collections;
 import gnu.jpdf.PDFJob;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+//Class created to show graph of
+//Students results for a chosen module
 public class GraphFrame extends JFrame {
+    //List of students
     ArrayList<Student> list;
+    //The module code
     String module;
+    //List of results used by graph class
     static ArrayList<Integer> tempList;
 
     public GraphFrame(ArrayList<Student> list, String module){
@@ -27,9 +31,11 @@ public class GraphFrame extends JFrame {
                 temp.add(i.getMark(module));
             }
         }
+        //Sorting grades to make trend of graph more clear
         temp.sort(Collections.reverseOrder());
         tempList = temp;
         graph graph = new graph();
+       //Action listener to save graph to pdf
         ActionListener ex = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,6 +46,7 @@ public class GraphFrame extends JFrame {
                 }
             }
         };
+       //Button to save pdf
         JButton button = new JButton("Export");
         button.addActionListener(ex);
         this.add(button,BorderLayout.NORTH);
@@ -48,10 +55,13 @@ public class GraphFrame extends JFrame {
         this.setSize(500,500);
         //this.setLocationRelativeTo(null);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //displaying frame
         this.setVisible(true);
 
     }
+    //method to export graph to pdf
     public void export(graph graph) throws IOException {
+        //allowing user to select file save location
         JFileChooser chooser = new JFileChooser(".");
         chooser.setDialogTitle("Save File");
         FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF files", "pdf");
@@ -59,52 +69,66 @@ public class GraphFrame extends JFrame {
         chooser.setFileFilter(filter);
         int select = chooser.showSaveDialog(this);
         if(select == JFileChooser.APPROVE_OPTION){
+            //Creating a file out put stream
             FileOutputStream stream = new FileOutputStream(chooser.getSelectedFile());
+            //Using the pdf job class from the gnujpdf api
             PDFJob job = new PDFJob(stream);
+            //Accessing the graphics of the pdf document
             Graphics g = job.getGraphics();
+            //Drawing bar chart onto graph
+            //Reducing scale to allow it to fit onto the page
             graph.bar(g,0.34);
+            //Closing connection to pdf document
             g.dispose();
             job.end();
             stream.close();
         }
         else{
+            //Showing error message if file location not allowed
             JOptionPane.showMessageDialog(this,"File could not be opened"
             , "File Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
-
+//class to draw graph onto frame
 class graph extends JPanel{
 
     public void paintComponent(Graphics g) {
         bar(g);
     }
-
+    //drawing a scatter graph of results
     public void scatter(Graphics g){
-        int start = 20;
-        int gap = 15;
-
+        int start = 20; //The position of first point
+        int gap = 15; //The distance to increase by each point
+        //iterating though list of marks
         for (int i = 0; i < GraphFrame.tempList.size(); i++) {
+           //plotting point
             g.fillOval(start, GraphFrame.tempList.get(i)*6, 5,5);
+            //drawing line from point to point
             if (i < GraphFrame.tempList.size() - 1){
                 g.drawLine(start,GraphFrame.tempList.get(i)*6,start + gap,GraphFrame.tempList.get(i+1)*6);
             }
-
+            //incrementing point
             start = start + gap ;
         }
     }
-
+    //drawing a bar chart of results
     public void bar(Graphics g){
-        int start = 10;
-        int gap = 15;
+        int start = 10; //The position of first point
+        int gap = 15;//The distance to increase by each point
+        //Converting to graphics 2d to allow me to make lines thicker
         Graphics2D g2 = (Graphics2D) g;
+       //making lines thicker
         g2.setStroke(new BasicStroke(3));
+        //Drawing axis
         g2.drawLine(start,this.getHeight() - 10,this.getWidth() - 10,this.getHeight() - 10); //horizontal
         g2.drawLine(start,this.getHeight() -10,start,0); // vertical
+        //incrementing point
         start += 10;
+        //returning the thickness of line to default
         g2.setStroke(new BasicStroke());
         for (int i = 0; i < GraphFrame.tempList.size(); i++) {
-            //g.fillOval(start, GraphFrame.tempList.get(i) * 6, 5, 5);
+            //drawing a bar for each value
             g2.drawLine(start - 5,this.getHeight() -10,start - 5,GraphFrame.tempList.get(i) * 6);
             g2.drawLine(start + 5,this.getHeight() -10,start + 5,GraphFrame.tempList.get(i) * 6);
             g2.drawLine(start -5,GraphFrame.tempList.get(i) * 6 , start + 5,GraphFrame.tempList.get(i)*6);
@@ -112,11 +136,15 @@ class graph extends JPanel{
         }
 
     }
-
+    //a function to draw a graph to a certain scale
     public void bar(Graphics g, Double scale){
+        //initializing graphics
         Graphics2D temp = (Graphics2D) g;
+        //setting scale
         temp.scale(scale,scale);
-        Graphics g2 = (Graphics)temp;
-        bar(g2);
+        //converting to correct data type for function
+        //calling g argument function
+        //runs as normal
+        bar((Graphics)temp);
     }
 }
