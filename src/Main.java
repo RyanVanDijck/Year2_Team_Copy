58206935890aba4fa67c438112251338ba7fe8dd
rv17643 <rv17643@essex.ac.uk>
2020-02-21 12:29:21 +0000

    Linux change

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
-	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            if (System.getProperty("os.name").equals("Linux")){
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            }
+            else {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            }
 
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
             // TODO Auto-generated catch block
