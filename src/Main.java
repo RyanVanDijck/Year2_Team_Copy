d235b82a9b1cf25cb1b3664d8f5856e27765f495
lc17176 <lc17176@essex.ac.uk>
2020-02-27 23:24:53 +0000

    Removed some redundant code and added to the statistics class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,15 +16,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        StudentMarksAnalyser main = new StudentMarksAnalyser();
+        new StudentMarksAnalyser();
 
-        // String file = "data.csv"; //Name of file
-
-        Statistics stats = new Statistics();
-        try{
-            stats.getStudentBest("2500001");
-        }catch(NullPointerException npe){
-
-        }
-}
+    }
 }
--- src/Statistics.java
+++ src/Statistics.java
@@ -7,103 +7,99 @@ public class Statistics {
 
     Read read;
 
-    private ArrayList<Student> marks;
-    private Student student;
-    String[][] array;
-    String[] headings;
-    int mark;
-    Map<String,Integer> module= new HashMap<>();
+    private ArrayList<Student> students;
+    private String[][] array;
+    private String[] headings;
+    private int mark;
+    Map<String, Integer> module = new HashMap<>();
 
-    public Statistics(Read read) throws IOException{
+    public Statistics(Read read) throws IOException {
         this.read = read;
-        marks = read.getData();
-        module.put(null,null);
+        students = read.getData();
+        module.put(null, null);
         array = new String[read.getData().size()][19];
         headings = new String[19];
-        for (int i = 0;i < read.getHeadings().size()-1;i++){
+        for (int i = 0; i < read.getHeadings().size() - 1; i++) {
             headings[i] = read.getHeadings().get(i);
         }
         headings[18] = "Average Mark";
-        for (int i = 0; i < read.getData().size() ; i++){
+        for (int i = 0; i < read.getData().size(); i++) {
             array[i][0] = read.getData().get(i).getRegNo();
             array[i][1] = read.getData().get(i).getExamNo();
             array[i][2] = read.getData().get(i).getStage();
-            for (int j=3; j<18; j++){
-                if (!(read.getData().get(i).getMark(headings[j]) == null)){
+            for (int j = 3; j < 18; j++) {
+                if (!(read.getData().get(i).getMark(headings[j]) == null)) {
                     array[i][j] = Integer.toString(read.getData().get(i).getMark(headings[j]));
-                }
-                else{
+                } else {
                     array[i][j] = "N/A";
                 }
             }
         }
     }
 
-    public Statistics() {}
-
-
     //Return the mean mark for the module
-    public double getMean(String module){
+    public double getMean(String module) {
         double mean = 0;
-        double total= 0;
+        double total = 0;
         int count = 0;
-        for (Student i : marks) {
-            if(i.getMark(module)!=null){
+        for (Student i : students) {
+            if (i.getMark(module) != null) {
                 total += i.getMark(module);
                 count++;
             }
         }
-        mean = total/count;
-        if (Double.isInfinite(mean) || Double.isNaN(mean)){
+        mean = total / count;
+        if (Double.isInfinite(mean) || Double.isNaN(mean)) {
             return 0.0;
         }
         return mean;
     }
 
     //Return Highest mark for that module
-    public double getMax(String module){
+    public double getMax(String module) {
         double Max = Integer.MIN_VALUE;
-        for(Student i:marks){
-            if(i.getMark(module)!=null){
+        for (Student i : students) {
+            if (i.getMark(module) != null) {
                 double mark = i.getMark(module);
-                if(mark>Max){
+                if (mark > Max) {
                     Max = mark;
                 }
             }
         }
-        if (Max ==Integer.MIN_VALUE) {
+        if (Max == Integer.MIN_VALUE) {
             return 0.0;
         }
         return Max;
     }
+
     //Return Lowest mark for that module;
-    public double getMin(String module){
+    public double getMin(String module) {
         double Min = Integer.MAX_VALUE;
-        for(Student i:marks){
-            if(i.getMark(module)!=null){
+        for (Student i : students) {
+            if (i.getMark(module) != null) {
                 double mark = i.getMark(module);
-                if(mark<Min){
-                    Min= mark;
+                if (mark < Min) {
+                    Min = mark;
                 }
             }
         }
-        if (Min == Integer.MAX_VALUE){
+        if (Min == Integer.MAX_VALUE) {
             return 0.0;
         }
         return Min;
     }
+
     //Return the Range value mark for that module;
-    public double getRange(String module){
-        return getMax(module)-getMin(module);
+    public double getRange(String module) {
+        return getMax(module) - getMin(module);
     }
 
 
-
     //Return Standard Deviation
-    public double getSD(String module){
+    public double getSD(String module) {
         double standardDeviation = 0.0;
         List<Integer> markslist = new ArrayList<Integer>();
-        for (Student i : marks) {
+        for (Student i : students) {
             if (i.getMark(module) != null) {
                 markslist.add(i.getMark(module));
             }
@@ -115,50 +111,62 @@ public class Statistics {
             double roundup = Math.round(s * 100) / 100D;
             standardDeviation += roundup;
         }
-        if (Double.isNaN(standardDeviation)){
+        if (Double.isNaN(standardDeviation)) {
             return 0.0;
         }
         return Math.sqrt(standardDeviation / (length - 1));
     }
 
+    public ArrayList<Student> rankStudents(String module) {
+        ArrayList<Student> ranked = new ArrayList<>();
+        for (Student s : students) if(s.getMark(module)!=null)ranked.add(s);
+        ranked.sort(Comparator.comparing(s -> s.getMark(module)));
+        Collections.reverse(ranked);
+        return ranked;
+    }
+
     //Method to return the best student in that module
     //Need to catch NullPointException in case that module has no value
     public Student getBestStudent(String module) {
-        double bestscore = getMax(module);
-        for (Student i : marks) {
-            if (i.getMark(module) != null) {
-                if (i.getMark(module) == bestscore) {
-                    student = i;
-                }
-            }
-        }
-       return student;
+        ArrayList<Student> ranked = rankStudents(module);
+        return ranked.get(0);
     }
 
     //Method to return the worst student in that module
     // Need to catch NullPointException in case that module has no value
-    public Student getWorstStudent(String module){
-        double lowest = getMin(module);
-        for (Student i : marks) {
-            if (i.getMark(module) != null) {
-                if (i.getMark(module) == lowest) {
-                    student = i;
-                }
-            }
-        }
-        return student;
+    public Student getWorstStudent(String module) {
+        ArrayList<Student> ranked = rankStudents(module);
+        return ranked.get(ranked.size()-1);
     }
 
-    public Map<String,Integer> getStudentBest(String regNo){
+    //Method to return the best student in that module
+    //Need to catch NullPointException in case that module has no value
+    public Student[] getBestStudents(String module, int n) {
+        ArrayList<Student> ranked = rankStudents(module);
+        Student[] best = new Student[n];
+        for(int i=0;i<n;i++)best[i]=ranked.get(i);
+        return best;
+    }
+
+    //Method to return the worst student in that module
+    // Need to catch NullPointException in case that module has no value
+    public Student[] getWorstStudents(String module, int n) {
+        ArrayList<Student> ranked = rankStudents(module);
+        Student[] worst = new Student[n];
+        for(int i=0;i<n;i++)worst[i]=ranked.get(i+(ranked.size()-n));
+        return worst;
+    }
+
+    public Map<String, Integer> getStudentBest(String regNo) {
         int best = Integer.MIN_VALUE;
-        for(int i=0;i<read.getData().size();i++){
-            if(array[i][0].equals(regNo)){
-                for(int j=3; j<18 ;j++){
-                    if(!(read.getData().get(i).getMark(headings[j])== null)){
+        for (int i = 0; i < read.getData().size(); i++) {
+            if (array[i][0].equals(regNo)) {
+                for (int j = 3; j < 18; j++) {
+                    if (!(read.getData().get(i).getMark(headings[j]) == null)) {
                         mark = read.getData().get(i).getMark(headings[j]);
-                        if(mark > best){
+                        if (mark > best) {
                             best = mark;
-                            module.replace(headings[j],best);
+                            module.replace(headings[j], best);
                         }
                     }
                 }
@@ -167,6 +175,7 @@ public class Statistics {
         System.out.println(module);
         return module;
     }
+}
 
 //    public Map<String, Integer> getStudentWorst(String regNo){
 //        int minValue = Integer.MAX_VALUE;
@@ -177,4 +186,4 @@ public class Statistics {
 //            }
 //    return worstmodule;
 //    }
-}
+
