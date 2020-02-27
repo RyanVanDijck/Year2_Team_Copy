c6b1ab9c2f76c841cc8f4ba4cc27e3e1655c0a40
lc17176 <lc17176@essex.ac.uk>
2020-02-27 23:25:19 +0000

    Removed test code

--- src/StudentMarksAnalyser.java
+++ src/StudentMarksAnalyser.java
@@ -13,22 +13,6 @@ public class StudentMarksAnalyser {
 	public StudentMarksAnalyser() throws IOException {
 		UI = new StudentMarksAnalyserUI("CE291_TEAM_21",this);//creating a window to show content
 		UI.setVisible(true);
-		Read read=new Read();
-		Statistics stats = new Statistics(read);
-		ArrayList<Student> students = stats.rankStudents("CE101-4-FY");
-		for(Student s:students){
-			System.out.println(s.getMark("CE101-4-FY"));
-		}
-		System.out.println(stats.getBestStudent("CE101-4-FY").getMark("CE101-4-FY"));
-		System.out.println(stats.getWorstStudent("CE101-4-FY").getMark("CE101-4-FY"));
-		Student[] best = stats.getBestStudents("CE101-4-FY",5);
-		for(Student s:best){
-			System.out.println(s.getMark("CE101-4-FY"));
-		}
-		Student[] worst = stats.getWorstStudents("CE101-4-FY",10);
-		for(Student s:worst){
-			System.out.println(s.getMark("CE101-4-FY"));
-		}
 	}
 
 	public void setRead(Read read) {
